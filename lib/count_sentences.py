6507d25a9c125710e1b15bac806fc8b68674cdb6
Mercy <maritimmercy8@gmail.com>
2025-05-13 11:51:14 +0300

    added property & setter

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,32 @@
-#!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self._value = ""
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if isinstance(new_value, str):
+            self._value = new_value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return self._value.endswith(".")
+
+    def is_question(self):
+        return self._value.endswith("?")
+
+    def is_exclamation(self):
+        return self._value.endswith("!")
+
+    def count_sentences(self):
+        if not self._value:
+            return 0
+        
+        sentences = re.split(r'[.!?]+', self._value)
+        return len([s for s in sentences if s.strip()])
